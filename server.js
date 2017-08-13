3a65a762ad32b49c881726ba85ed60b3adf8733c
Vikas Prasad <vikasprasad.prasad@gmail.com>
2017-08-13 11:09:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1', function (req, res) {
+  res.send("Article 1 is requestedd and will be handle here.");
+});
+
+app.get('/article2', function (req, res) {
+  res.send("Article 2 is requestedd and will be handle here.");
+});
+
+app.get('/article3', function (req, res) {
+  res.send("Article 3 is requestedd and will be handle here.");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
