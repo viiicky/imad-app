3ffb667466ae39509693655e367d92e2baee40be
viiicky <vikasprasad.prasad@gmail.com>
2017-08-13 12:43:31 +0530

    Reuse code

--- server.js
+++ server.js
@@ -5,20 +5,87 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+	'article-one':{
+		title : 'Article One',
+		heading : 'Article One',
+		date : 'Aug 13, 2017',
+		content : `<p>
+                    This is the contet for my article one. This is the contet for my article one. This is the contet for my article one.
+                </p>
+                
+                <p>
+                    This is the contet for my article one. This is the contet for my article one. This is the contet for my article one.
+                </p>
+                
+                <p>
+                    This is the contet for my article one. This is the contet for my article one. This is the contet for my article one.
+                </p>`
+	},
+	'article-two':{
+		title : 'Article Two',
+		heading : 'Article Two',
+		date : 'Aug 14, 2017',
+		content :`<p>
+                    This is the content for my second article.
+				</p>`
+	},
+	'article-three':{
+		title : 'Article Trhee',
+		heading : 'Article Three',
+		date : 'Aug 15, 2017',
+		content :`<p>
+                    This is the content for my third article.
+				</p>`
+	}
+};
+
+function createHtml(data){
+	var title = data.title;
+	var heading = data.heading;
+	var content = data.content;
+	var date = data.date;
+	var htmlTemplate = `
+	<html>
+    <head>
+        <title>
+            ${title} | Vikas Prasad
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+            <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+				${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html>
+`
+
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article1', function (req, res) {
+app.get('/:articleName', function (req, res) {
+	var articleName = req.params.articleName;
+	console.log(articleName);
+	res.send(createHtml(articles[articleName]));
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article2', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article3', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
